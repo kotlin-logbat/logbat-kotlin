5aa2f91f1202f1a085e4c98094d18962d3d14a38
Hae-gun <newchk610@gmail.com>
2025-01-07 23:25:00 +0900

    feature: @JvmStatic 으로 임시 변경

--- src/main/kotlin/info/logbat/common/util/UUIDUtil.kt
+++ src/main/kotlin/info/logbat/common/util/UUIDUtil.kt
@@ -3,17 +3,21 @@ package info.logbat.common.util
 import java.nio.ByteBuffer
 import java.util.*
 
-object UUIDUtil {
-    private val UUID_BYTE_LENGTH = 16
-    fun uuidStringToBytes(uuidStr: String):ByteArray{
-        val uuid = UUID.fromString(uuidStr)
-        val byteBuffer = ByteBuffer.wrap(ByteArray(UUID_BYTE_LENGTH))
-        byteBuffer.putLong(uuid.mostSignificantBits)
-        byteBuffer.putLong(uuid.leastSignificantBits)
-        return byteBuffer.array();
-    }
-    fun  bytesToUuidString(bytes: ByteArray) :String{
-        val byteBuffer = ByteBuffer.wrap(bytes);
-        return UUID(byteBuffer.getLong(), byteBuffer.getLong()).toString()
+class UUIDUtil {
+    companion object {
+        private val UUID_BYTE_LENGTH = 16
+        @JvmStatic
+        fun uuidStringToBytes(uuidStr: String):ByteArray{
+            val uuid = UUID.fromString(uuidStr)
+            val byteBuffer = ByteBuffer.wrap(ByteArray(UUID_BYTE_LENGTH))
+            byteBuffer.putLong(uuid.mostSignificantBits)
+            byteBuffer.putLong(uuid.leastSignificantBits)
+            return byteBuffer.array();
+        }
+        @JvmStatic
+        fun  bytesToUuidString(bytes: ByteArray) :String{
+            val byteBuffer = ByteBuffer.wrap(bytes);
+            return UUID(byteBuffer.getLong(), byteBuffer.getLong()).toString()
+        }
     }
 }
