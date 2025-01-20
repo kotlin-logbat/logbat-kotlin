aec5dd7fafb1cf08f3e9c75c0398f3c1d72618d0
Hae-gun <newchk610@gmail.com>
2025-01-20 22:36:25 +0900

    Test: 프로세서 테스트 코드 작성/Mocking 확인

--- src/test/kotlin/info/logbat/domain/log/repository/AsyncMultiProcessorTest.kt
+++ src/test/kotlin/info/logbat/domain/log/repository/AsyncMultiProcessorTest.kt
@@ -1,6 +1,9 @@
 package info.logbat.domain.log.repository
 
+import com.zaxxer.hikari.HikariDataSource
 import info.logbat.domain.log.domain.Log
+import info.logbat.domain.log.domain.enums.Level
+import info.logbat.domain.log.domain.values.LogData
 import info.logbat.domain.log.queue.ReentrantLogQueue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -12,6 +15,18 @@ import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.beans.factory.ObjectProvider
 import org.springframework.jdbc.core.JdbcTemplate
 import org.assertj.core.api.Assertions.assertThat
+import org.mockito.BDDMockito
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.*
+import org.springframework.jdbc.core.RowMapper
+import java.time.LocalDateTime
+import javax.sql.DataSource
+
+class MySuspendMockFun{
+    suspend fun suspendFun(list:List<Log>){
+        println("mock test")
+    }
+}
 
 @ExtendWith(MockitoExtension::class)
 @DisplayName("AsyncMultiProcessorTest는")
@@ -26,18 +41,29 @@ class AsyncMultiProcessorTest{
 
     private lateinit var asyncMultiProcessor: AsyncMultiProcessor<Log>
 
+    private val expectedLog = Log(
+        id = 1L,
+        appId = 1L,
+        level = Level.from(0),
+        data = LogData.from("Test log data"),
+        timestamp = LocalDateTime.of(2021, 1, 1, 0, 0, 0)
+    )
+
+    private lateinit var expectedRetrant: ReentrantLogQueue<Log>
 
     @BeforeEach
     fun setUp() {
+        val mockDataSource = mock(HikariDataSource::class.java)
+        jdbcTemplate = spy(JdbcTemplate(mockDataSource))
         asyncMultiProcessor = AsyncMultiProcessor(3,5000,3000,jdbcTemplate, objectProvider)
     }
 
     @Test
     @DisplayName("잘 생성된다.")
     fun initTest(){
-        val suspendFun = {data:List<Log> ->
-            println(data)
-        }
+        var suspendFun = mock(MySuspendMockFun::class.java)
+        given(objectProvider.getObject())
+            .willReturn(expectedRetrant)
         assertThat(asyncMultiProcessor.init { suspendFun }).isNotNull
     }
 
